53b80d5d4d64391f2a92042acd9a53418f8fdb85
laxleary <133721313+laxleary@users.noreply.github.com>
2023-06-26 14:32:51 -0400

    First edits

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,23 @@
 
+import os
+import csv
+
+#create lists to store data
+date = []
+profit_losses = []
+
+#open and read the csv file
+csvpath = os.path.join(r"PyBank\Resources\budget_data.csv")
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csvheader= next(csvreader)
+    print(csvheader)
+
+    for row in csvreader:
+
+        date.append(row[0])
+
+        profit_losses.append(row[1])
+
+
