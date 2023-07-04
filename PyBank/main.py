04f1366257d9cbd16a86453bbfd342ca08a624af
laxleary <133721313+laxleary@users.noreply.github.com>
2023-07-04 14:07:42 -0400

    Commited PyBank, again

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 budget_file = Path("PyBank","Resources", "budget_data.csv")
 
-budget_df = pd.read_csv(budget_file, encoding = "ISO-8859-1")
 
 months = budget_df["Date"].unique()
 months_count=len(months)
