d6cf7f98bb8df85816e8880e6f0b61ad7db091f5
laxleary <133721313+laxleary@users.noreply.github.com>
2023-07-04 14:09:10 -0400

    Attempt to match git with local

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 budget_file = Path("PyBank","Resources", "budget_data.csv")
 
+budget_df = pd.read_csv(budget_file, encoding = "ISO-8859-1")
 
 months = budget_df["Date"].unique()
 months_count=len(months)
