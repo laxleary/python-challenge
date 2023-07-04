ee7e927d512ce3ab57dc72ec335936add8826d0f
laxleary <133721313+laxleary@users.noreply.github.com>
2023-07-04 14:11:54 -0400

    Error with commit fixes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,12 +1,48 @@
 
-import pandas as pd
-from pathlib import Path
+import os
+import csv
 
-budget_file = Path("PyBank","Resources", "budget_data.csv")
 
-budget_df = pd.read_csv(budget_file, encoding = "ISO-8859-1")
+#create lists to store data
+date = []
+profit_losses = []
 
-months = budget_df["Date"].unique()
-months_count=len(months)
-total = budget_df["Profit/Losses"].sum()
-print(total)
+#open and read the csv file
+csvpath = os.path.join("PyBank","Resources","budget_data.csv")
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csvheader= next(csvreader)
+
+    for row in csvreader:
+
+        date.append(row[0])
+
+        profit_losses.append(int(row[1]))
+
+changes = []
+
+for x in range(85):
+    changes.append(profit_losses[x+1]-profit_losses[x])
+    
+
+total_months= len(date)
+Total = sum(profit_losses)
+average_change = sum(changes)/len(changes)
+greatest_increase = max(changes)
+greatest_decrease = min(changes)
+
+for i in range(85):
+    if changes[i]==max(changes):
+        gi_name = date[i+1]
+    elif changes[i]==min(changes):
+        gd_name = date[i+1]
+
+
+financial_analysis = f"Financial Analysis \n -------------- \n Total Months: {total_months} \n Total: ${Total} \n Average Change: ${average_change} \n Greatest Increase in Profits: {gi_name} (${greatest_increase}) \n Greatest Decrease in Profits: {gd_name} (${greatest_decrease})"
+output_path = os.path.join("PyBank", "Analysis", "financial_analysis.txt")
+with open(output_path, 'w') as txtfile:
+    txtfile.write(f"{financial_analysis}")
+print(f"{financial_analysis}")
+
+print("Success!")
